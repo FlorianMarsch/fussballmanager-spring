d28bcd6d38df9e04578408d4e7692cb36bdddb40
florianmarsch <dekontaminator@googlemail.com>
2020-09-20 20:17:13 +0200

    add logger

--- src/main/kotlin/de/florianmarsch/spring/fussballmanager/RefrenceDataRestController.kt
+++ src/main/kotlin/de/florianmarsch/spring/fussballmanager/RefrenceDataRestController.kt
@@ -1,13 +1,17 @@
 package de.florianmarsch.spring.fussballmanager
 
 import de.florianmarsch.spring.fussballmanager.persistence.*
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.*
 
 
 @RestController
 class ReferenceDataRestController {
+
+	var logger: Logger = LoggerFactory.getLogger(ReferenceDataRestController::class.java)
 
 	@Value("\${community}")
 	private val community: Int? = null
@@ -53,11 +57,15 @@ class ReferenceDataRestController {
 
 	@PostMapping("/api/goals/{match}")
 	fun  saveGoals(@RequestBody goals:List<Goal>, @PathVariable(value="match")  match:Int) {
+		logger.info("Save ${goals.size} Goals for ${match}.")
 		val findByMatch = goalRepo.findByMatch(match)?.filterNotNull()
+		logger.info("Already has ${findByMatch.size} Goals for ${match}.")
 		findByMatch?.let {
 			val deleted = it.toMutableList().apply {
 				removeAll(goals)
 			}
+
+			logger.info("Need to delete ${deleted.size} Goals for ${match}.")
 			goalRepo.deleteAll(deleted)
 		}
 		goalRepo.saveAll(goals)
