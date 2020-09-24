69df152bf1ec0b443ed6460b48cd315d575b1d8b
florianmarsch <dekontaminator@googlemail.com>
2020-09-24 21:40:53 +0200

    change id on repository

--- src/main/kotlin/de/florianmarsch/spring/fussballmanager/ranking/RankingRepository.kt
+++ src/main/kotlin/de/florianmarsch/spring/fussballmanager/ranking/RankingRepository.kt
@@ -5,6 +5,6 @@ import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface RankingRepository : PagingAndSortingRepository<Ranking?, Gameday?> {
+interface RankingRepository : PagingAndSortingRepository<Ranking?, RankingId?> {
 
 }
